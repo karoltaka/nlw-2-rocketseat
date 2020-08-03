3b52460ab12e880efdc6c73125831244803d346d
karoltaka <takahagassi.dev@gmail.com>
2020-08-03 17:27:19 -0300

    Initial files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <button type="button">Clica em mim</button>
+    </div>
+  );
+}
+
+export default App;
