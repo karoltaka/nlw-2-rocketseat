bf884ac8f0eac048af26b3a3a975b48338695c13
karoltaka <takahagassi.dev@gmail.com>
2020-08-06 17:30:17 -0300

    Connecting back-end to front-end

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../../services/api";
 
 import { Link } from "react-router-dom";
 
@@ -11,6 +12,17 @@ import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 import "./styles.css";
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get("connections").then((response) => {
+      const { total } = response.data;
+
+      console.log(total);
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -35,7 +47,7 @@ function Landing() {
           </Link>
         </div>
         <span className="total-connections">
-          Total de 200 conexões já realizadas
+          Total de {totalConnections} conexões já realizadas
           <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
